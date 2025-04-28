4f06e054e90ba66fe50b3eaed82cc662599068c7
Diego Mansilla <diiego.mansilla@gmail.com>
2025-04-28 14:11:54 -0300

    Fix staticfiles

--- publicidadweb/settings.py
+++ publicidadweb/settings.py
@@ -120,10 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # si usás una carpeta local de static
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
